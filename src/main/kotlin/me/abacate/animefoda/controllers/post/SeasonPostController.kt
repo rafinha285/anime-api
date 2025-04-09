0bc47fdbb5cbce15c63fba2f05789483de050a26
Rafael <jcschrott10@gmail.com>
2025-04-09 04:40:40 -0300

    Season create mapping added

--- src/main/kotlin/me/abacate/animefoda/controllers/post/SeasonPostController.kt
+++ src/main/kotlin/me/abacate/animefoda/controllers/post/SeasonPostController.kt
@@ -1,32 +1,42 @@
 package me.abacate.animefoda.controllers.post
 
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
+import me.abacate.animefoda.enums.RoleName
+import me.abacate.animefoda.errors.UnauthorizedResponse
 import me.abacate.animefoda.services.JWTService
 import me.abacate.animefoda.models.Season
 import me.abacate.animefoda.repositories.SeasonRepository
+import me.abacate.animefoda.request.NewSeasonRequest
 import me.abacate.animefoda.response.ApiResponse
+import me.abacate.animefoda.services.UserService
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.HttpClientErrorException.Unauthorized
+import java.util.*
 
 @RestController
-@RequestMapping("/p/seasons")
-class SeasonPostController (
+@RequestMapping("/p/season")
+class SeasonPostController(
     private val seasonRepository: SeasonRepository,
-    private val jwtService: JWTService
+    private val jwtService: JWTService,
+    private val userService: UserService
 ) {
     @PostMapping("/new")
     fun newSeason(
-        @RequestBody season: Season,
-        request: HttpServletRequest,
-        response: HttpServletResponse,
-        @CookieValue(name = "token") token: String
+        @RequestBody body: NewSeasonRequest,
+        @AuthenticationPrincipal jwt: Jwt
     ):ApiResponse<String?> {
-        try {
-//            jwtUtil.checkToken(token, request, response)
-            seasonRepository.save(season)
-            return ApiResponse()
-        }catch (e: Exception){
-            return ApiResponse(success = false, message = e.message)
+        val userUUID = UUID.fromString(jwt.subject)
+        if(!userService.containsRole(userUUID,RoleName.ROLE_ADMIN)){
+            throw UnauthorizedResponse()
         }
+//            jwtUtil.checkToken(token, request, response)
+        val season = Season(
+            name = body.name,
+            index = body.index,
+            animeId = body.anime_id,
+        )
+        seasonRepository.save(season)
+        return ApiResponse()
     }
 }
